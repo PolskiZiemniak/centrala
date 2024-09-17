e5de858e58a4e00473ddc4c34445cc25d2f10108
PolskiZiemniak <polskiziemniak2018@gmail.com>
2024-09-17 14:25:17 +0200

    Updated index.html and added font

---
+++ index.php
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<script src="assets/js/main.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
