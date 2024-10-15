84cb5803f75c5e6f05d7f02e1b85cb16bd54a0f0
PolskiZiemniak <polskiziemniak2018@gmail.com>
2024-10-15 14:20:22 +0200

    Added sliding sidebar and json file with sidebar structure

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+function showMenu(){
+    document.getElementById("sidebar").style.width = "200px";
+    document.getElementById("sidebar-top").style.width = "200px";
+    document.getElementById("sidebar-top-text").style.color = "black";
+}
