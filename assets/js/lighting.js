8b3945eb99d89126f6b6208463319406fe79df27
PolskiZiemniak <polskiziemniak2018@gmail.com>
2024-11-12 13:51:13 +0100

    Working sidebar with content

---
+++ assets/js/lighting.js
@@ -0,0 +1,3 @@
+$('#lighting').on('click', 'button', function (e) {
+    $('p').html('Zmieniona treść');
+});
--- index.php
+++
@@ -1,4 +0,0 @@
-<link rel="stylesheet" href="assets/css/style.css">
-<link rel="stylesheet" href="assets/css/bootstrap.min.css">
-<script src="assets/js/main.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
