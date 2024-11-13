88fbccdbd295087f1a05bb905b9600aa8da24adf
PolskiZiemniak <polskiziemniak2018@gmail.com>
2024-11-13 12:40:02 +0100

    Added ajax

--- assets/js/lighting.js
+++ assets/js/lighting.js
@@ -1,3 +1,22 @@
 $('#lighting').on('click', 'button', function (e) {
-    $('p').html('Zmieniona treść');
+    //$('p').html('Zmieniona treść');
+
+    $.ajax({
+        url: "ajax.php",
+        type: 'post',
+        data: {'user' : 'test'},
+        beforeSend: function(){
+            console.log("start");
+        },
+        complete: function(){
+            console.log("complete request");
+        },
+        success: function(data){
+            $('p').text(data);
+        },
+        error: function(){
+
+        }
+    
+    });
 });
